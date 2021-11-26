00e4ebee3e645b0ba357f8120395488b0b9bd6b4
iroviram <ram.villarr@gmail.com>
2021-11-25 21:17:25 -0600

    proyecto

--- Projects/DevF/ecommerce-react-project-2/src/views/Profile/Profile.js
+++ Projects/DevF/ecommerce-react-project-2/src/views/Profile/Profile.js
@@ -2,25 +2,14 @@ import axios from 'axios';
 import React,{useEffect,useState} from 'react'
 import Navbar from '../../Components/Navbar/Navbar'
 import protect from '../../Utils/protect';
+import payload from '../../Utils/payload';
 
 function Profile() {
 
 
-    /* var myHeaders = new Headers();
-    myHeaders.append("Authorization", "JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYwZTc4NjVkZjU5NjdmMDAxN2ZmOTM3YSIsInJvbGUiOiJDVVNUT01FUiIsImV4cCI6MTYyNTg3NDA3NywiaWF0IjoxNjI1Nzg3Njc3fQ.TKeEOklbDSBxBY2H0pMuaZpDfppiAIgjfQOr24tIQWU");
-
-    var requestOptions = {
-        method: 'GET',
-        headers: myHeaders,
-        redirect: 'follow'
-    };
-
-    axios.get("https://ecomerce-master.herokuapp.com/api/v1/user", requestOptions)
-        .then(response => response.data)
-        .then(result => console.log(result))
-        .catch(error => console.log('error', error)); */
-
-        /* const [user,setUser] = useState([])
+/*         const user = payload()
+        const token = window.localStorage.getItem("token");
+        const [user,setUser] = useState([])
 
         useEffect(() => {
             const token = window.localStorage.getItem("token");
